56c6281ee8b9fdaca6ca43dd481c31b36cc503fa
Sai Vineeth Tallapudi <64919520+saivineeth100@users.noreply.github.com>
2024-11-12 13:00:21 +0000

    added tb report

---
+++ erpnext_extended_reports/accounts_ext/report/trial_balance_(ext)/trial_balance_(ext).js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Sai Vineeth Tallapudi and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Trial Balance (ext)"] = {
+	"filters": [
+
+	]
+};
