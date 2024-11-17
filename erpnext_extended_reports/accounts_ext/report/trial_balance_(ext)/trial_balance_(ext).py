3d87aa2ab068337040747417de2d2f34236d3920
Sai Vineeth Tallapudi <64919520+saivineeth100@users.noreply.github.com>
2024-11-17 09:22:45 +0000

    added tb fields

--- erpnext_extended_reports/accounts_ext/report/trial_balance_(ext)/trial_balance_(ext).py
+++ erpnext_extended_reports/accounts_ext/report/trial_balance_(ext)/trial_balance_(ext).py
@@ -5,5 +5,66 @@
 
 
 def execute(filters=None):
-	columns, data = [], []
+	columns, data = get_columns(), []
 	return columns, data
+
+
+def get_columns():
+	return [
+		{
+			"fieldname": "account",
+			"label": _("Account"),
+			"fieldtype": "Link",
+			"options": "Account",
+			"width": 300,
+		},
+		{
+			"fieldname": "currency",
+			"label": _("Currency"),
+			"fieldtype": "Link",
+			"options": "Currency",
+			"hidden": 1,
+		},
+		{
+			"fieldname": "opening_debit",
+			"label": _("Opening (Dr)"),
+			"fieldtype": "Currency",
+			"options": "currency",
+			"width": 120,
+		},
+		{
+			"fieldname": "opening_credit",
+			"label": _("Opening (Cr)"),
+			"fieldtype": "Currency",
+			"options": "currency",
+			"width": 120,
+		},
+		{
+			"fieldname": "debit",
+			"label": _("Debit"),
+			"fieldtype": "Currency",
+			"options": "currency",
+			"width": 120,
+		},
+		{
+			"fieldname": "credit",
+			"label": _("Credit"),
+			"fieldtype": "Currency",
+			"options": "currency",
+			"width": 120,
+		},
+		{
+			"fieldname": "closing_debit",
+			"label": _("Closing (Dr)"),
+			"fieldtype": "Currency",
+			"options": "currency",
+			"width": 120,
+		},
+		{
+			"fieldname": "closing_credit",
+			"label": _("Closing (Cr)"),
+			"fieldtype": "Currency",
+			"options": "currency",
+			"width": 120,
+		},
+	]
